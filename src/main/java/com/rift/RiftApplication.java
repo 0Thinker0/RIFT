3280ad1c1702bab05afd1a0ef4d54a9706b4cd89
0Thinker0 <gabrykron2@gmail.com>
2022-01-13 16:56:25 +0100

    Update

--- src/main/java/com/rift/RiftApplication.java
+++ src/main/java/com/rift/RiftApplication.java
@@ -8,7 +8,7 @@ public class RiftApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RiftApplication.class, args);
-		System.out.println("e");
+		System.out.println("Test invio");
 	}
 
 }
